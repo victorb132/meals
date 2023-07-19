b761bc103318dfc26304b9a65387a804d5eff039
Victor Novais <victor96novais@gmail.com>
2023-07-19 17:02:44 -0300

    feat/passing-parameters

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({super.key});
+  final Category category;
+
+  CategoriesMealsScreen(this.category, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Receitas'),
+        title: Text(category.title),
       ),
       body: Center(
-        child: Text('Receitas por categoria'),
+        child: Text('Receitas por categoria ${category.id}'),
       ),
     );
   }
