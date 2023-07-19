08e1be201531722699e6acc497432446e8903530
Victor Novais <victor96novais@gmail.com>
2023-07-19 17:32:29 -0300

    feat/init-meal-component

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
 
@@ -20,7 +21,7 @@ class CategoriesMealsScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: categoryMeals.length,
         itemBuilder: ((context, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(categoryMeals[index]);
         }),
       ),
     );
