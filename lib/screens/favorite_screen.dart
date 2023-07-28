1a4d87deff95e1a4ff64c16478d07e535a73d5ee
Victor Novais <victor96novais@gmail.com>
2023-07-28 17:54:11 -0300

    feat/teste

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -6,7 +6,7 @@ class FavoriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Minhas refeições favoritas!'),
+      child: Text('Minhas refeições favoritas.'),
     );
   }
 }
