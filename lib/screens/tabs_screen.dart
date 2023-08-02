662452a56df2691532cd00e45e215f64e3a803b5
Victor Novais <victor96novais@gmail.com>
2023-08-02 14:35:06 -0300

    feat/tab-bar-inferios

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,35 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_screens.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+  final List<Map<String, Object>> _screens = [
+    {'title': 'Lista de Categorias', 'screen': const CategoriesScreens()},
+    {'title': 'Meus favoritos', 'screen': const FavoriteScreen()},
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Vamos Cozinhar?'),
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Categorias',
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: 'Favoritos',
-                ),
-              ],
-            ),
+        appBar: AppBar(
+          title: Text(
+            _screens[_selectedScreenIndex]['title'] as String,
           ),
-          body: const TabBarView(
-            children: [
-              CategoriesScreens(),
-              FavoriteScreen(),
-            ],
-          )),
+        ),
+        body: _screens[_selectedScreenIndex]['screen'] as Widget,
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).colorScheme.secondary,
+          currentIndex: _selectedScreenIndex,
+          type: BottomNavigationBarType.shifting,
+          onTap: _selectScreen,
+          items: const [
+            BottomNavigationBarItem(
+              backgroundColor: Colors.pink,
+              icon: Icon(Icons.category),
+              label: 'Categorias',
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Colors.pink,
+              icon: Icon(Icons.star),
+              label: 'Favoritos',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
